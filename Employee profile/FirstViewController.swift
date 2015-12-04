64c07c2066dce3ff91fb63cf2dae05b4da26d749
ramya radhakrishnan <ramyaradhakrishnan@ramyas-macbook-pro.local>
2015-12-03 18:42:48 -0800

    Initial Commit

---
+++ Employee profile/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Employee profile
+//
+//  Created by ramya radhakrishnan on 12/3/15.
+//  Copyright © 2015 ramya radhakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
